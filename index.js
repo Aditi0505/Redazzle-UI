73dd98d18f5d74b659b8341d9c46786666b91637
Aditi0505 <aditi5may1997@gmail.com>
2022-02-17 22:01:55 +0530

    feat: added sidebar open-close functionality

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const btn_toggle = document.querySelector("#toggle");
+const nav_toggle = document.querySelector(".hamburger-icon");
 const btn_off = document.querySelector(".fa-toggle-off");
-
+const nav_close = document.querySelector(".nav-close");
+const container = document.querySelector(".container");
+const sidebar = document.querySelector(".sidebar");
 handleClick = () => {
   btn_toggle.classList.contains("fa-toggle-off")
     ? handleDarkMode()
@@ -36,4 +39,16 @@ handleLightMode = () => {
   localStorage.setItem("theme", "light");
 };
 
+handleNavClick = () => {
+  container.classList.remove("container");
+  container.classList.add("container-open");
+  nav_close.classList.remove("hide");
+};
+handleClose = () => {
+  container.classList.remove("container-open");
+  container.classList.add("container");
+  nav_close.classList.remove("hide");
+};
 btn_toggle.addEventListener("click", handleClick);
+nav_toggle.addEventListener("click", handleNavClick);
+nav_close.addEventListener("click", handleClose);
